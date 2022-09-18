e763b1bc7cb9c389c74b12b36d470af4f290db61
Chengyu Yang <chengyu_yang@outlook.com>
2022-09-18 21:41:28 +0800

    <feat> demo for button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import Button, { ButtonSize, ButtonType } from './components/button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <hr/>
+        <Button disabled>Hello World</Button>
+        <Button btnType={ButtonType.Link} href='https://www.baidu.com' disabled>Hello World</Button>
         <code>
           const a = 'b'
         </code>
